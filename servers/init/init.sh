e9bc8909831e36d7943a5fab36f0325c19262921
Cuong Nguyen <cuongnb14@gmail.com>
2020-09-30 13:46:31 +0700

    Update init.sh

--- servers/init/init.sh
+++ servers/init/init.sh
@@ -2,8 +2,9 @@
 
 git clone https://github.com/zsh-users/antigen.git ~/.antigen.repo
 
-wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc ~/.zshrc
-wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc.local ~/.zshrc.local
+wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc -O ~/.zshrc
+wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc.local -O ~/.zshrc.local
+wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.gitconfig -O ~/.gitconfig
 
 # vim config
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
