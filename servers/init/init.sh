3311bea7907f6c13a4b81584975a60ba45201868
Cuong Nguyen <cuongnb14@gmail.com>
2020-11-25 14:30:12 +0700

    update init script

--- servers/init/init.sh
+++ servers/init/init.sh
@@ -1,4 +1,4 @@
-sudo apt install zsh curl git wget xclip autojump python3-pip
+sudo apt install zsh curl git wget xclip autojump python3-pip vim
 
 sudo pip3 install virtualenvwrapper
 
@@ -7,6 +7,7 @@ git clone https://github.com/zsh-users/antigen.git ~/.antigen.repo
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc -O ~/.zshrc
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc.local -O ~/.zshrc.local
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.gitconfig -O ~/.gitconfig
+wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.autoenv -O ~/.autoenv
 
 # vim config
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
