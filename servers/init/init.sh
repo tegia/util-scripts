167ab6d84d5a5ef524af358131a6256590476905
Cuong Nguyen <cuongnb14@gmail.com>
2020-11-27 11:07:40 +0700

    fix zsh config

--- servers/init/init.sh
+++ servers/init/init.sh
@@ -7,7 +7,7 @@ git clone https://github.com/zsh-users/antigen.git ~/.antigen.repo
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc -O ~/.zshrc
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.zshrc.local -O ~/.zshrc.local
 wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.gitconfig -O ~/.gitconfig
-wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.autoenv -O ~/.autoenv
+wget https://raw.githubusercontent.com/cuongnb14/util-scripts/master/servers/init/.autoenv.sh -O ~/.autoenv.sh
 
 # vim config
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
