3c557ae61b5d59c1da473a55f6ea8e478d6db7e9
nghiatt <tuongthenghia@gmail.com>
2022-02-13 10:26:13 +0700

    Update init.sh

--- servers/init-ubuntu/init.sh
+++ servers/init-ubuntu/init.sh
@@ -15,3 +15,13 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 sudo chsh -s $(which zsh) $(whoami)
+
+# install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+
